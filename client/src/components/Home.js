80bf34d295ce187c58fd36f04159501d1500687f
Matthew Bruder <mbruder@students.kennesaw.edu>
2019-08-07 17:20:52 -0400

    update 3 7-8

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,6 +6,7 @@ const Home = (props) => {
       <h1>Mannavest is cool.</h1>
       <h2>About Us</h2>
       <p>Its a site/app where you can raise capital for your business or donate to others.</p>
+      <p>Trust me. This shit is the future.</p>
     </div>
   )
 }
